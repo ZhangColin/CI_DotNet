c75affaf5ba376dddf9fad2b3579a63489d93ccf
Zhang Colin <zhang_colin@163.com>
2016-11-08 15:54:51 +0800

    Complete msbuild config

---
+++ CISample/CISample.Web.UnitTests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+using CISample.Web.Controllers;
+using NUnit.Framework;
+
+namespace CISample.Web.UnitTests.ControllerTests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private HomeController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _controller = new HomeController();
+        }
+
+        [Test]
+        public void IndexActionReturnsView()
+        {
+            // Act
+            var viewResult = _controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(viewResult);
+        }
+
+        [Test]
+        public void AboutActionReturnsView()
+        {
+            // Act
+            var viewResult = _controller.About() as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(viewResult);
+            Assert.AreEqual("Your application description page.", viewResult.ViewBag.Message);
+        }
+    }
+}
